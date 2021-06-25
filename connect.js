d4e35809101b1d8bdec21189c8a87e4ef6fd8761
Reza <73782101+dragonshck@users.noreply.github.com>
2021-06-25 22:05:27 +0700

    Minor tweaks
    Optimizing ajax for search and detail changes

--- connect.js
+++ connect.js
@@ -31,14 +31,32 @@ function showCards(vg) {
         <h5 class="m-0">${vg.name}</h5>
     </div><img class="img-fluid card-img w-100 d-block rounded-0" src="${vg.background_image}">
     <div class="card-body">
-    <h6 class="card-subtitle mb-2 text-muted">Ratings : ${vg.rating}</h6>
-        <p class="card-text"><strong>${vg.genres[0].name}</strong></p>
+    <h6 class="card-subtitle mb-2 text-muted">Platforms : ${vg.platforms[0].platform['name']}</h6>
+        <p class="card-text"><strong>${vg.genres[0].name}</strong> | ${vg.released}</p>
     </div>
 </div>
 </div>`;
 };
 
-{/* <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#exampleModal" type="button" style="font-family: 'Bebas Neue', serif;background: var(--red);" data-game="${vg.id}">show details</a> */}
-// $('.modal-detail-button').on('click', function () {
-//     console.log($(this).data('vgid'));
-// });
+// function showGameDetail(vgx) {
+//     return `<div class="modal-body" id="detail-game">
+//     <div class="container-fluid">
+//         <div class="row">
+//             <div class="col-md-3">
+//                 <img src="${vgx.background_image_additional}" class="img-fluid">
+//             </div>
+//             <div class="col-md">
+//               <ul class="list-group">
+//                   <li class="list-group-item"><h4>${vgx.name}</h4></li>
+//                   <li class="list-group-item"><strong>Description : </strong> <br> ${vgx.description}</li>
+//                   <li class="list-group-item"><strong>Rating : </strong> ${vgx.metacritic} </li>
+//                   <li class="list-group-item"><strong>Developers : </strong> ${vgx.developers[0].name} </li>
+//                   <li class="list-group-item"><strong>URL : </strong> ${vgx.website} </li>
+//                 </ul>
+//             </div>
+//         </div>
+//     </div>
+//   </div>`;
+// };
+
+// <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#exampleModal" type="button" style="font-family: 'Bebas Neue', serif;background: var(--red);" data-game="${vg.id}">show details</a>
