730606e67253e19b8c6e5b952c1bcf71c1a60912
Reza <73782101+dragonshck@users.noreply.github.com>
2021-06-25 18:07:00 +0700

    Fixing some bugs
    Added page retrieve limit too

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: 'https://api.rawg.io/api/games?key=3f274333aa8b4d41b43c0c399d27ac74&dates=2021-01-01,2021-12-31&ordering=-added',
+    url: 'https://api.rawg.io/api/games?key=3f274333aa8b4d41b43c0c399d27ac74&dates=2021-01-01,2021-12-31&ordering=-added&page_size=15',
     success: results => {
         const vgames = results.results;
         let cards = '';
@@ -12,7 +12,7 @@ $.ajax({
 
 $('.btn-search').on('click', function () {
     $.ajax({
-        url: 'https://api.rawg.io/api/games?key=3f274333aa8b4d41b43c0c399d27ac74&search=' + $('.search-field').val(),
+        url: 'https://api.rawg.io/api/games?key=3f274333aa8b4d41b43c0c399d27ac74&page_size=15&search=' + $('.search-field').val(),
         success: results => {
             const vgames = results.results;
             let cards = '';
@@ -31,9 +31,14 @@ function showCards(vg) {
         <h5 class="m-0">${vg.name}</h5>
     </div><img class="img-fluid card-img w-100 d-block rounded-0" src="${vg.background_image}">
     <div class="card-body">
-        <p class="card-text">${vg.genres[0].name} | ${vg.released}</p>
-        <a href="#" class="btn btn-primary modal-detail-button" id="search-button" type="button" style="font-family: 'Bebas Neue', serif;background: var(--red);">show details</a>
+    <h6 class="card-subtitle mb-2 text-muted">Ratings : ${vg.rating}</h6>
+        <p class="card-text"><strong>${vg.genres[0].name}</strong></p>
     </div>
 </div>
 </div>`;
 };
+
+{/* <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#exampleModal" type="button" style="font-family: 'Bebas Neue', serif;background: var(--red);" data-game="${vg.id}">show details</a> */}
+// $('.modal-detail-button').on('click', function () {
+//     console.log($(this).data('vgid'));
+// });
